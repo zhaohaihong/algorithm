8e51ae4c30045b857f443ba01ecc3d0bdb80f2e0
zhaohaihong <andromeda.zhao@gmail.com>
2021-08-04 22:45:04 +0800

    Fix: daily practice

---
+++ src/Tree/Special/Trie.java
@@ -0,0 +1,80 @@
+package Tree.Special;
+
+class Trie {
+
+    private class Node {
+        Node[] childs = new Node[26];
+        boolean isLeaf;
+    }
+
+    private Node root = new Node();
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        insert(word, root);
+    }
+
+    private void insert(String word, Node node) {
+        if (node == null) {
+            return;
+        }
+        if (word.length() == 0) {
+            node.isLeaf = true;
+            return;
+        }
+        int index = indexForChar(word.charAt(0));
+        if (node.childs[index] == null) {
+            node.childs[index] = new Node();
+        }
+        insert(word.substring(1), node.childs[index]);
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        return search(word, root);
+    }
+
+    private boolean search(String word, Node node) {
+        if (node == null) {
+            return false;
+        }
+        if (word.length() == 0) {
+            return node.isLeaf;
+        }
+        int index = indexForChar(word.charAt(0));
+        return search(word.substring(1), node.childs[index]);
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        return startsWith(prefix, root);
+    }
+
+    private boolean startsWith(String prefix, Node node) {
+        if (node == null) {
+            return false;
+        }
+        if (prefix.length() == 0) {
+            return true;
+        }
+        int index = indexForChar(prefix.charAt(0));
+        return startsWith(prefix.substring(1), node.childs[index]);
+    }
+
+    private int indexForChar(char c) {
+        return c - 'a';
+    }
+}
